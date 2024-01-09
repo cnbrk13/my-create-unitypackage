06f656cbe53d1ea70d25708f847048d874157fe4
Can Berk T <53381468+cnbrk13@users.noreply.github.com>
2024-01-09 00:01:22 +0000

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,29 @@
 import { getInput, info } from "@actions/core";
 import create from "unitypackage";
-import { readFile } from "node:fs/promises";
+import { readFile, access } from "node:fs/promises";
 import { chdir } from "node:process";
+import { constants } from "node:fs";
+
 const IsNotNullOrWhiteSpace = (value) => value && value.trim();
 const Split = (linesConcat) => {
     const splits = linesConcat.split(/\r\n|\n|\r/);
     return splits.filter(IsNotNullOrWhiteSpace);
 };
+
 const workingFolder = getInput("working-folder", { required: false });
 const output = getInput("package-path", { required: true });
 const projectFolder = getInput("project-folder", { required: false }) ?? "./";
 const includeFilesPath = getInput("include-files", { required: true });
+
+if (workingFolder != null) {
+    try {
+        await access(workingFolder, constants.F_OK);
+        chdir(workingFolder);
+    } catch (error) {
+        throw new Error(`The directory ${workingFolder} does not exist or cannot be accessed.`);
+    }
+}
+
 const data = await readFile(includeFilesPath, { encoding: "utf-8" });
 const metaFiles = Split(data);
 await create(metaFiles, projectFolder, output, info);
